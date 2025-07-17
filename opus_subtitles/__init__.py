1082c88919c883383b26d6de01552123e72a3d71
lbdx <lbeaudoux@gmail.com>
2025-07-17 09:08:09 +0200

    Add logging configuration

--- opus_subtitles/__init__.py
+++ opus_subtitles/__init__.py
@@ -1,8 +1,12 @@
+import logging
+
 from .opus_subtitles import (
     download_raw_subtitle_zip,
     extract_subtitle_txt_files,
     read_subtitle_lines,
 )
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 __all__ = [
     "download_raw_subtitle_zip",
